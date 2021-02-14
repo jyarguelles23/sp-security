675e66db136125f27e37bf5696a7069f9ffe0843
jyarguelles23 <jyarguelles23@gmail.com>
2021-02-14 19:34:13 -0300

    Configuring UserDetails Service for in Memory database Login

--- src/main/java/com/example/springsecurity/Security/AppSecurityConfig.java
+++ src/main/java/com/example/springsecurity/Security/AppSecurityConfig.java
@@ -1,10 +1,15 @@
 package com.example.springsecurity.Security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /* This class We set all the security configurations for our application */
 @Configuration
@@ -15,7 +20,6 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
      // Authorize any request, must be authenticated and mecanism is basic authentication.
-
        http
                .authorizeRequests()
                .antMatchers("/","index","/css/*","/js/*","/ts/*")
@@ -24,5 +28,20 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                .authenticated()
                .and()
                .httpBasic();
+    }
+
+    /*This is how u retrive your users from database*/
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+      UserDetails anna =  User.builder()
+                .username("annasmith")
+                .password("slipknot")
+                .roles("STUDENT") // role student
+                .build();
+      // for in memory database user
+      return new InMemoryUserDetailsManager(
+              anna
+      );
     }
 }
